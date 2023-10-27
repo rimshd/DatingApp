7b5d52785ad11dc98276c7b924e5d3a353e00143
rimshd <rimshad7025@gmail.com>
2023-10-27 09:06:02 +0530

    End of section8

--- API/Extensios/ApplicationServiceExtensions.cs
+++ API/Extensios/ApplicationServiceExtensions.cs
@@ -17,6 +17,8 @@ namespace API.Extensios
             });
             services.AddCors();
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             return services;
          } 
