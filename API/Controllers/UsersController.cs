4482097dde58770c6fbc5ee83b4bb3be71a72a9a
rimshd <rimshad7025@gmail.com>
2023-11-06 09:55:50 +0530

    End of section 13

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,7 +2,9 @@
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Extensios;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -29,9 +31,19 @@ public class UsersController : BaseApiController
 
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUSers()
+    public async Task<ActionResult<PagedList<MemberDto>>> GetUSers([FromQuery]UserParams userParams)
     {
-        var users = await _userRepository.GetMembersAsync();
+        var currentUser = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+        userParams.CurrentUsername = currentUser.UserName;
+
+        if (string.IsNullOrEmpty(userParams.Gender))
+        {
+            userParams.Gender = currentUser.Gender == "male" ? "female" : "male";
+        }
+
+        var users = await _userRepository.GetMembersAsync(userParams);
+
+        Response.AddPaginationHeader(new PaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages));
 
         return Ok(users);
     }
